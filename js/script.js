07f6aeb1429b8b296fd6b60f46a00ebe62aae319
pietrostraneo <straneo_p@yahoo.it>
2023-12-01 15:23:23 +0100

    Script completed

---
+++ js/script.js
@@ -0,0 +1,59 @@
+function randomNumber(array){
+    let check = false;
+    let random;
+
+    while(!check){
+        random = Math.floor(Math.random() * 100 + 1);
+
+        if(!array.includes(random)){
+            check = true;
+        }
+    }
+
+    return random
+}
+
+function array(nums){
+    let arr = []
+
+    for(let i=0; i<nums; i++){
+        arr.push(randomNumber(arr));
+    }
+
+    return arr
+}
+
+const numbers = 5;
+let game = array(numbers)
+console.log(game)
+
+let title = document.querySelector(`h2`)
+let memo = document.querySelector(`h5`);
+let correct = document.getElementById(`score`)
+let num_ok = document.getElementById(`num_ok`)
+let points = 0;
+
+memo.innerText = game
+
+setTimeout(function(){
+    title.innerText = ""
+    memo.innerText = ""
+}, 30000)
+
+let user_num = []
+
+setTimeout(function(){
+    while(user_num.length < 5){
+        let guess = parseInt(prompt(`Inserisci un numero da indovinare`))
+        user_num.push(guess)
+    }
+    console.log(user_num)
+    for(let i=0; i<numbers; i++){
+        if(game.includes(user_num[i])){
+            points++
+            correct.innerText = `Hai indovinato i seguenti (${points}) numeri: `
+            num_ok.innerText += " " + user_num[i]
+        }
+    }
+}, 31000)
+
